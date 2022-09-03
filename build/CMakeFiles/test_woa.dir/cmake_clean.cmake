80e9c2a7248956494b8df1aba9df23048041f33b
lirun-sat <18B918089@stu.hit.edu.cn>
2022-09-03 14:20:59 +0200

    check woa optimization algorithm

---
+++ build/CMakeFiles/test_woa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/test_woa"
+  "../bin/test_woa.pdb"
+  "CMakeFiles/test_woa.dir/src/main_1.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_woa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
